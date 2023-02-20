661ca7263ff154392948f8f3af073233eb69c08c
nullopt <alqohol@hotmail.com>
2023-02-20 17:19:44 +0000

    ClipboardCopyButtonContainer refactor

--- src/components/ClipboardCopyButtonContainer/ClipboardCopyButtonContainer.tsx
+++ src/components/ClipboardCopyButtonContainer/ClipboardCopyButtonContainer.tsx
@@ -1,14 +1,14 @@
-import { Tooltip } from "@mui/material"
+import { Tooltip } from "@mui/material";
+import { canCopyImagesToClipboard } from "copy-image-clipboard";
 
-export const ClipboardCopyButtonContainer = ({ className, children }: { className: string, children: JSX.Element }) => {
+const warningMsg =
+  "Copy Image to Clipboard functionality is only available in Chrome, Edge, Safari, and Opera. " +
+  "If using Firefox, you can set `dom.events.asyncClipboard.clipboardItem` to`true` in about: config.";
+
+export const ClipboardCopyButtonContainer = ({ className, children }: { className: string; children: JSX.Element }) => {
   return (
-    <Tooltip
-      className={className}
-      title="Copy Image to Clipboard functionality is only available in Chrome, Edge, Safari, and Opera. If using Firefox, you can set `dom.events.asyncClipboard.clipboardItem` to `true`."
-    >
-      <span>
-        {children}
-      </span>
+    <Tooltip className={className} title={!canCopyImagesToClipboard() && warningMsg}>
+      <span>{children}</span>
     </Tooltip>
   );
 };
