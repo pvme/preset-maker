4c5d1a2fdc339477540aae030a65a9483e3680c5
Rcm37 <46052381+Rcm37@users.noreply.github.com>
2023-03-15 22:26:58 -0400

    parsing the sanitized and stringified preset to json for upload

--- src/components/HeaderBar/headerBarApi.ts
+++ src/components/HeaderBar/headerBarApi.ts
@@ -3,8 +3,9 @@ import axios from "axios";
 const apiUrl = "https://us-central1-pvmebackend.cloudfunctions.net/uploadPreset";
 
 export const UploadPreset = async (data: string) => {
+  data = (typeof(data) === "object" ? data : JSON.parse(data));
   const response = await axios.post(apiUrl, data, {
-    withCredentials: true,
+    withCredentials: false,
   });
   return response.data;
 };
