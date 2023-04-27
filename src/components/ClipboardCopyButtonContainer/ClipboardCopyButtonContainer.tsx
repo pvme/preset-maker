aeb228aadd5ff03eeabd2814236b17649852bd49
codemati <colintinney@gmail.com>
2023-04-27 10:36:57 -0700

    fix: check for clipboarditem

--- src/components/ClipboardCopyButtonContainer/ClipboardCopyButtonContainer.tsx
+++ src/components/ClipboardCopyButtonContainer/ClipboardCopyButtonContainer.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Tooltip } from '@mui/material';
 import { canCopyImagesToClipboard } from 'copy-image-clipboard';
 
+const canUseClipboard = () => Boolean(window.ClipboardItem) && canCopyImagesToClipboard;
+
 const warningMsg =
   'Copy Image to Clipboard functionality is only available in Chrome, Edge, Safari, and Opera. ' +
   'If using Firefox, you can set `dom.events.asyncClipboard.clipboardItem` to`true` in about: config.';
 
 export const ClipboardCopyButtonContainer = ({ className, children }: { className: string, children: JSX.Element }): JSX.Element => {
   return (
-    <Tooltip className={className} title={!canCopyImagesToClipboard() && warningMsg}>
+    <Tooltip className={className} title={!canUseClipboard() && warningMsg}>
       <span>{children}</span>
     </Tooltip>
   );
