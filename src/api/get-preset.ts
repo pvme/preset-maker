aa0d56246563f3fabe405daf54af4d2742004198
x222 <113437965+rsnx222@users.noreply.github.com>
2025-12-14 17:16:04 +0000

    emojis_v2 checked - pushing new get

--- src/api/get-preset.ts
+++ src/api/get-preset.ts
@@ -3,9 +3,14 @@
 import axios from "axios";
 import { type Preset } from "../schemas/preset";
 import { normalizePreset } from "../redux/store/reducers/normalizePreset";
+import { getDevHeaders } from "./get-headers";
+import { FunctionURLs } from "./function-urls";
 
 export async function getPreset(id: string): Promise<Preset> {
-  const { data } = await axios.get(`/api/presets/${id}`);
+  const { data } = await axios.get(
+    `${FunctionURLs.getPreset}?id=${encodeURIComponent(id)}`,
+    { headers: getDevHeaders() }
+  );
+
   return normalizePreset(data);
 }
-
